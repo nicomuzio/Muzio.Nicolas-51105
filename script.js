00f52e7b12abde00e5fc64684336000f9e1e6ba8
Nicolas Muzio <nicolas.muzio@gmail.com>
2023-03-28 11:52:59 -0300

    modificacion de preentrega N°3

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
-const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numberChars = "0123456789";
-const symbolChars = "!@#$%^&*()_+-=[]{}\\|;:'\",.<>/?";
-let passwords = [];
+// Variables de elementos del DOM
+const passwordLength = document.getElementById("password-length");
+const includeUppercase = document.getElementById("include-uppercase");
+const includeLowercase = document.getElementById("include-lowercase");
+const includeNumbers = document.getElementById("include-numbers");
+const includeSymbols = document.getElementById("include-symbols");
+const generateBtn = document.getElementById("generate-btn");
+const resultBox = document.getElementById("result-box");
 
-function generatePassword() {
-  const length = document.getElementById("length").value;
-  const includeLowercase = document.getElementById("lowercase").checked;
-  const includeUppercase = document.getElementById("uppercase").checked;
-  const includeNumbers = document.getElementById("numbers").checked;
-  const includeSymbols = document.getElementById("symbols").checked;
-  const socialNetwork = prompt("Por favor, ingresa el nombre de la red social o sitio web:");
+// Evento de clic en el botón Generar contraseña
+generateBtn.addEventListener("click", function () {
+  const length = parseInt(passwordLength.value);
+  const uppercase = includeUppercase.checked;
+  const lowercase = includeLowercase.checked;
+  const numbers = includeNumbers.checked;
+  const symbols = includeSymbols.checked;
+  
+  let password = generatePassword(length, uppercase, lowercase, numbers, symbols);
+  
+  if(password) {
+    resultBox.innerHTML = `<p>Tu nueva contraseña es: <strong>${password}</strong></p>`;
+    storePassword(password);
+  } else {
+    resultBox.innerHTML = "";
+    alert("Debe seleccionar al menos una opción para generar la contraseña.");
+  }
+});
 
-
-  let charSet = "";
-  if (includeLowercase) {
-    charSet += lowercaseChars;
-  }
-  if (includeUppercase) {
-    charSet += uppercaseChars;
-  }
-  if (includeNumbers) {
-    charSet += numberChars;
-  }
-  if (includeSymbols) {
-    charSet += symbolChars;
+// Función para generar la contraseña
+function generatePassword(length, uppercase, lowercase, numbers, symbols) {
+  // Asegurarse de que el largo de la contraseña esté dentro del rango permitido
+  if (length < 6 || length > 15) {
+    alert("El largo de la contraseña debe estar entre 6 y 15 caracteres.");
+    return "";
   }
 
+  // Crear el conjunto de caracteres a utilizar para generar la contraseña
+  let chars = "";
+  if (uppercase) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (lowercase) chars += "abcdefghijklmnopqrstuvwxyz";
+  if (numbers) chars += "0123456789";
+  if (symbols) chars += "!@#$%^&*()_+-=[]{}\\|;:'\",.<>/?";
+
+  // Generar la contraseña
   let password = "";
   for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charSet.length);
-    password += charSet[randomIndex];
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
   }
 
-  passwords.push({ socialNetwork, password });
-
-  alert(`Tu contraseña para ${socialNetwork} es: ${password}`);
-
-  document.getElementById("export").style.display = "inline";
+  return password;
 }
 
-function exportToExcel() {
-  const worksheetName = "Contraseñas";
-  const workbook = XLSX.utils.book_new();
-  const worksheet = XLSX.utils.json_to_sheet(passwords);
-  XLSX.utils.book_append_sheet(workbook, worksheet, worksheetName);
-
-  const fileName = "contraseñas.xlsx";
-  XLSX.writeFile(workbook, fileName);
+// Función para almacenar la contraseña en el localStorage
+function storePassword(password) {
+  const storedPasswords = JSON.parse(localStorage.getItem("passwords")) || [];
+  storedPasswords.push(password);
+  localStorage.setItem("passwords", JSON.stringify(storedPasswords));
 }
-
-document.getElementById("generate").addEventListener("click", generatePassword);
-document.getElementById("export").addEventListener("click", exportToExcel);
